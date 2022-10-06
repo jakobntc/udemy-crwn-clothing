b9bd5ef50727e16eafcb866ca649162d0ab7ac60
Jakob Clapsaddle <Jakobntc.95@gmail.com>
2022-10-06 00:39:28 -0400

    Adding comments for clarity.

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,30 +1,27 @@
 import { useState } from "react";
 
-const defaultFormFields = {
-  displayName: "",
+const initialFormFields = {
+  displayName: "Sam",
   email: "",
   password: "",
   confirmPassword: "",
 }
 
 const SignUpForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-
-  const { displayName, email, password, confirmPassword } = formFields;
-  console.log(formFields);
+  const [formFields, setFormFields] = useState(initialFormFields); // Creating a state variable
+  const { displayName, email, password, confirmPassword } = formFields; // Destructoring
 
   const handleChange = (event) => {
     const { name, value } = event.target;
+    console.log("Event for ", name, " with new value ", value);
 
-    setFormFields({ ...formFields, [name]: value });
+    setFormFields({ ...formFields, [name]: value }); // ... = Foreach element of formFields 
   }
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {
-
-      }}>
+      <form onSubmit={() => { }}>
         <label>Display Name</label>
         <input type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
