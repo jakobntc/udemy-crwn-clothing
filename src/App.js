60318a74ecb90105c09c431ae56d1a01af8ffec6
Jakob Clapsaddle <Jakobntc.95@gmail.com>
2022-10-08 20:26:27 -0400

    Updating routing for auth

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<Authentication />} />
+        <Route path="auth" element={<Authentication />} />
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
